ddecf79ae5774178a077c7f5e1e3005fa4015e32
andreamarciano <andrea.marciano.66@gmail.com>
2025-08-23 11:25:22 +0200

    set up complete

--- app/index.tsx
+++ app/index.tsx
@@ -5,7 +5,7 @@ import "./styles/global.css";
 export default function Home() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Modifica da dev</Text>
+      <Text className="text-xl font-bold text-blue-500">Home</Text>
       <StatusBar style="auto" />
     </View>
   );
